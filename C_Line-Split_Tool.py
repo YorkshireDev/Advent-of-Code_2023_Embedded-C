203009bd560ee55761fbb9612494a421be471553
YorkshireDev <YorkshireDev@protonmail.com>
2024-02-10 01:26:02 +0000

    Show the longest line

--- C_Line-Split_Tool.py
+++ C_Line-Split_Tool.py
@@ -2,13 +2,22 @@ def main():
 
     input_list:  list = []
     output_list: list = []
+    
+    longest_str: list = ["", 0]
 
     with open("Input.TXT", 'r') as input_file:
         input_list.extend(input_file.readlines())
 
     for input_line in input_list:
-        output_line: str = "	\"" + input_line.split('\n')[0] + "`\""
+        input_str: str = input_line.split('\n')[0]
+        output_line: str = "	\"" + input_str + "`\""
         output_list.append(output_line)
+        
+        if len(input_str) > longest_str[1]:
+            longest_str[0] = input_str
+            longest_str[1] = len(input_str)
+            
+    print(f"Longest String: {longest_str[0]} | Length: {longest_str[1]}")
 
     output_list[-1] += ";"
 
